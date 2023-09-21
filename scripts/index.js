3a83ce412b365c7beaaf35df26d6a1a135ccff1e
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-09-21 14:32:28 -0300

    criando o array de personagens

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,8 @@
-console.log("Hello World");
+const members = [
+  { name: "Peter Quill", id: "peter" },
+  { name: "Gamora Titan", id: "gamora" },
+  { name: "Drax o Destruidor", id: "drax" },
+  { name: "Mantis", id: "mantis" },
+  { name: "Rocket Raccoon", id: "rocket" },
+  { name: "I am Groot", id: "groot" },
+];
