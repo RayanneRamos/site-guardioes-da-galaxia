128565e86144ba687764f3874061db160f509c40
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-09-21 15:36:35 -0300

    mudando as cores dos nomes de acordo com os personagens

--- scripts/index.js
+++ scripts/index.js
@@ -10,13 +10,14 @@ const members = [
 let activeMember = 0;
 const images = document.getElementById("images");
 const menu = document.getElementById("menu");
+const memberName = document.getElementById("member__name");
 
 function changeStatusButtons() {
-  const prev = document.getElementById("button-prev");
+  const prev = document.getElementById("button__prev");
   const isFirst = activeMember === 0;
   prev.disabled = isFirst;
 
-  const next = document.getElementById("button-next");
+  const next = document.getElementById("button__next");
   const isLast = activeMember === members.length - 1;
   next.disabled = isLast;
 }
@@ -28,6 +29,7 @@ function navigationMember(direction) {
   console.log(activeMember, member);
 
   images.style.transform = `translateY(${-100 * activeMember}vh)`;
+  memberName.classList = member.id;
 
   changeStatusButtons();
 }
