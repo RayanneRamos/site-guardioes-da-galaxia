6139ac83eb24c183a030c9fd6c09a83a337d2014
Rayanne Ramos <rayanneramosoficial@gmail.com>
2023-09-21 14:55:53 -0300

    criando a função de mudar os personagens de acordo com os botões

--- scripts/index.js
+++ scripts/index.js
@@ -10,6 +10,16 @@ const members = [
 let activeMember = 0;
 const images = document.getElementById("images");
 
+function changeStatusButtons() {
+  const prev = document.getElementById("button-prev");
+  const isFirst = activeMember === 0;
+  prev.disabled = isFirst;
+
+  const next = document.getElementById("button-next");
+  const isLast = activeMember === members.length - 1;
+  next.disabled = isLast;
+}
+
 function navigationMember(direction) {
   activeMember = activeMember + direction;
   const member = members[activeMember];
@@ -17,4 +27,6 @@ function navigationMember(direction) {
   console.log(activeMember, member);
 
   images.style.transform = `translateY(${-100 * activeMember}vh)`;
+
+  changeStatusButtons();
 }
